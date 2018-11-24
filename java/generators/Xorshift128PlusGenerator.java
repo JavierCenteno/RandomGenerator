bc7dd866966b6b28755217d0a3ca3d32ee96b8c8
JavierCenteno <jacenve@telefonica.net>
2018-11-25 00:13:29 +0100

    Added xorshift128+

---
+++ java/generators/Xorshift128PlusGenerator.java
@@ -0,0 +1,96 @@
+package generators;
+
+import api.RandomGenerator;
+import util.ByteConverter;
+
+/**
+ * Implementation of a xorshift128+ PRNG with a state of 128 bits.
+ * 
+ * @author Javier Centeno Vega <jacenve@telefonica.net>
+ * @version 1.0
+ * @see api.RandomGenerator
+ * @since 1.0
+ * 
+ */
+public class Xorshift128PlusGenerator implements RandomGenerator {
+
+	// -----------------------------------------------------------------------------
+	// Class fields
+
+	/**
+	 * Size of this generator's state in bytes.
+	 */
+	public static final int STATE_SIZE = 16;
+	/**
+	 * Size of this generator's seed in bytes.
+	 */
+	public static final int SEED_SIZE = STATE_SIZE;
+
+	// -----------------------------------------------------------------------------
+	// Instance fields
+
+	/**
+	 * State of this generator.
+	 */
+	private long[] state;
+
+	// -----------------------------------------------------------------------------
+	// Instance initializers
+
+	/**
+	 * Constructs a generator with a randomly chosen seed provided by the default
+	 * seed generator.
+	 */
+	public Xorshift128PlusGenerator() {
+		this(DEFAULT_SEED_GENERATOR.generateBytes(SEED_SIZE));
+	}
+
+	/**
+	 * Constructs a generator with the given seed.
+	 * 
+	 * @param seed
+	 *                 A seed.
+	 * @throws IllegalArgumentException
+	 *                                      If the seed is too short.
+	 */
+	public Xorshift128PlusGenerator(byte[] seed) {
+		setSeed(seed);
+	}
+
+	// -----------------------------------------------------------------------------
+	// Instance methods
+
+	@Override
+	public int getSeedSize() {
+		return SEED_SIZE;
+	}
+
+	@Override
+	public int getStateSize() {
+		return STATE_SIZE;
+	}
+
+	@Override
+	public byte[] getState() {
+		return ByteConverter.longsToBytes(this.state);
+	}
+
+	@Override
+	public void setState(byte[] state) {
+		if (state.length < STATE_SIZE) {
+			throw new IllegalArgumentException();
+		}
+		this.state = ByteConverter.bytesToLongs(state);
+	}
+
+	@Override
+	public long generateUniformLong() {
+		long state0 = state[0];
+		long state1 = state[1];
+		state[0] = state1;
+		state0 ^= state0 << 23;
+		state[1] = state0 ^ state1 ^ (state0 >> 17) ^ (state1 >> 26);
+		return state[1] + state1;
+	}
+
+}
