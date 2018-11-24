7cfc6cb6761d6d3177aba27c843e28269a78c787
JavierCenteno <jacenve@telefonica.net>
2018-11-24 23:40:56 +0100

    Added Xorwow generator

---
+++ java/generators/XorwowGenerator.java
@@ -0,0 +1,116 @@
+package generators;
+
+import java.security.SecureRandom;
+
+import api.RandomGenerator;
+import util.ByteConverter;
+
+/**
+ * Implementation of a Xorwow PRNG with a state of 160 bits.
+ * 
+ * @author Javier Centeno Vega <jacenve@telefonica.net>
+ * @version 1.0
+ * @see api.RandomGenerator
+ * @since 1.0
+ * 
+ */
+public class XorwowGenerator implements RandomGenerator {
+
+	// -----------------------------------------------------------------------------
+	// Class fields
+
+	/**
+	 * Size of this generator's state in integers.
+	 */
+	public static final int STATE_SIZE_INTEGERS = 5;
+	/**
+	 * Size of this generator's state in bytes.
+	 */
+	public static final int STATE_SIZE = STATE_SIZE_INTEGERS * Integer.BYTES;
+	/**
+	 * Size of this generator's seed in bytes.
+	 */
+	public static final int SEED_SIZE = STATE_SIZE;
+
+	// -----------------------------------------------------------------------------
+	// Instance fields
+
+	/**
+	 * State of this generator.
+	 */
+	private int[] state;
+
+	// -----------------------------------------------------------------------------
+	// Instance initializers
+
+	/**
+	 * Constructs a generator with a randomly chosen seed as given by SecureRandom.
+	 * 
+	 * @see SecureRandom
+	 */
+	public XorwowGenerator() {
+		this(DEFAULT_SEED_GENERATOR.generateBytes(SEED_SIZE));
+	}
+
+	/**
+	 * Constructs a generator with the given seed.
+	 * 
+	 * @param seed
+	 *                 A seed.
+	 * @throws IllegalArgumentException
+	 *                                      If the seed is too short.
+	 */
+	public XorwowGenerator(byte[] seed) {
+		setSeed(seed);
+	}
+
+	// -----------------------------------------------------------------------------
+	// Instance methods
+
+	@Override
+	public int getSeedSize() {
+		return SEED_SIZE;
+	}
+
+	@Override
+	public int getStateSize() {
+		return STATE_SIZE;
+	}
+
+	@Override
+	public byte[] getState() {
+		return ByteConverter.integersToBytes(this.state);
+	}
+
+	@Override
+	public void setState(byte[] state) {
+		if (state.length < STATE_SIZE) {
+			throw new IllegalArgumentException();
+		}
+		this.state = ByteConverter.bytesToIntegers(state);
+	}
+
+	@Override
+	public int generateUniformInteger() {
+		int t = state[3];
+		t ^= t >>> 2;
+		t ^= t << 1;
+		state[3] = state[2];
+		state[2] = state[1];
+		int s = state[0];
+		state[1] = s;
+		t ^= s;
+		t ^= s << 4;
+		state[0] = t;
+		state[4] += 362437;
+		return t + state[4];
+	}
+
+	@Override
+	public long generateUniformLong() {
+		long int0 = generateUniformInteger() & 0x00000000FFFFFFFFL;
+		long int1 = generateUniformInteger() & 0x00000000FFFFFFFFL;
+		return int0 << 32 | int1;
+	}
+
+}
