d599c0b097feb87b389666eaec43872cbf3ce3d8
JavierCenteno <jacenve@telefonica.net>
2018-11-25 00:12:48 +0100

    Added xorshift1024*

---
+++ java/generators/Xorshift1024StarGenerator.java
@@ -0,0 +1,132 @@
+package generators;
+
+import api.RandomGenerator;
+import util.ByteConverter;
+
+/**
+ * Implementation of a xorshift1024* PRNG with a state of 1024 bits.
+ * 
+ * @author Javier Centeno Vega <jacenve@telefonica.net>
+ * @version 1.0
+ * @see api.RandomGenerator
+ * @since 1.0
+ * 
+ */
+public class Xorshift1024StarGenerator implements RandomGenerator {
+
+	// -----------------------------------------------------------------------------
+	// Class fields
+
+	/**
+	 * Size of this generator's state in integers.
+	 */
+	public static final int STATE_SIZE_LONGS = 16;
+	/**
+	 * Size of this generator's state in bytes.
+	 */
+	public static final int STATE_SIZE = STATE_SIZE_LONGS * Long.BYTES;
+	/**
+	 * Size of this generator's seed in bytes.
+	 */
+	public static final int SEED_SIZE = STATE_SIZE;
+	/**
+	 * Size of this generator's state, including the index, in bytes.
+	 */
+	public static final int FULL_STATE_SIZE = Integer.BYTES + STATE_SIZE;
+
+	// -----------------------------------------------------------------------------
+	// Instance fields
+
+	/**
+	 * Index used to access elements of this generator.
+	 */
+	private int index;
+	/**
+	 * State of this generator. This field is simply used for the generation of
+	 * values returned by the generator and doesn't include other generator
+	 * parameters that may affect the returned result, so this may not be equivalent
+	 * to what the method getState() returns.
+	 */
+	private long[] state;
+
+	// -----------------------------------------------------------------------------
+	// Instance initializers
+
+	/**
+	 * Constructs a generator with a randomly chosen seed provided by the default
+	 * seed generator.
+	 */
+	public Xorshift1024StarGenerator() {
+		this(DEFAULT_SEED_GENERATOR.generateBytes(SEED_SIZE));
+	}
+
+	/**
+	 * Constructs a generator with the given seed.
+	 * 
+	 * @param seed
+	 *                 A seed.
+	 * @throws IllegalArgumentException
+	 *                                      If the seed is too short.
+	 */
+	public Xorshift1024StarGenerator(byte[] seed) {
+		setSeed(seed);
+	}
+
+	// -----------------------------------------------------------------------------
+	// Instance methods
+
+	@Override
+	public int getSeedSize() {
+		return SEED_SIZE;
+	}
+
+	@Override
+	public int getStateSize() {
+		return FULL_STATE_SIZE;
+	}
+
+	@Override
+	public void setSeed(byte[] seed) {
+		if (seed.length < STATE_SIZE) {
+			throw new IllegalArgumentException();
+		}
+		this.index = 0;
+		this.state = ByteConverter.bytesToLongs(seed);
+	}
+
+	@Override
+	public byte[] getState() {
+		byte[] indexBytes = ByteConverter.integerToBytes(index);
+		byte[] stateBytes = ByteConverter.longsToBytes(this.state);
+		byte[] fullState = new byte[indexBytes.length + stateBytes.length];
+		System.arraycopy(indexBytes, 0, fullState, 0, indexBytes.length);
+		System.arraycopy(stateBytes, 0, fullState, indexBytes.length, stateBytes.length);
+		return fullState;
+	}
+
+	@Override
+	public void setState(byte[] state) {
+		if (state.length < FULL_STATE_SIZE) {
+			throw new IllegalArgumentException();
+		}
+		byte[] indexBytes = new byte[Integer.BYTES];
+		byte[] stateBytes = new byte[STATE_SIZE];
+		System.arraycopy(state, 0, indexBytes, 0, indexBytes.length);
+		System.arraycopy(state, indexBytes.length, stateBytes, 0, stateBytes.length);
+		this.index = ByteConverter.bytesToInteger(indexBytes);
+		this.state = ByteConverter.bytesToLongs(stateBytes);
+	}
+
+	@Override
+	public long generateUniformLong() {
+		long state0 = state[index];
+		index = (index + 1) % STATE_SIZE_LONGS;
+		long state1 = state[index];
+		state1 ^= state1 << 31;
+		state1 ^= state1 >>> 11;
+		state1 ^= state0 ^ (state0 >>> 30);
+		state[index] = state1;
+		return state1 * 1181783497276652981L;
+	}
+
+}
